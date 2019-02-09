13d9a7482e1a12aee7ba760ed5f365e819ac4cb2
Sándor Juhász <sandor.juhasz.1983@gmail.com>
2019-02-09 08:14:19 +0100

    Fixing typo in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt-get install git
 mkdir -p ~/projects
 cd ~/projects || exit
-git checkout https://github.com/sandor-juhasz/ubuntu-devbox.git
+git clone https://github.com/sandor-juhasz/ubuntu-devbox.git
 cd ubuntu-devbox || exit
 
 ./setup-ansible
